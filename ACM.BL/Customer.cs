b3621f39256e569233f055190a25fcbee88a465a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-26 13:59:20 +0200

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+        public string FirstName { get; set; } 
+        // Shortened version of property below, called Auto-Generated property.
+
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
