bc1b07844ba06c3348d8b9a6339d1b0e707dcd63
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-26 14:14:12 +0200

    Module 3: Testing the Class: Valid Values

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
- * Building Entity Classes: 
- * Using Snippets
+ * Testing the Class: 
+ * Valid Values
  */
 namespace ACM.BL
 {
---
+++ ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02dfe425-3c70-47b1-9a3c-3581a112d04f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
