bbf9798e915cb203ffd7f38b11fd93775c089a0e
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-27 19:40:21 +0200

    Module 6: Composition: References

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -12,19 +12,22 @@ namespace ACM.BL
     {
         // After instantiating a first constructor with any parameter, 
         // we need to write a code for a default constructor if we want him to appear at a runtime.
-        public Customer()
+        public Customer() : this(0) 
         {
-
+        // Adding a colon and referencing a custom constructor from below is called "Constructor Chaining". 
+        // Use it any time one constructor needs to call another. In this case, the default constructor calls
+        // the custom one, providing a default customerId value of 0. Now the Adress List is always initialized.
+        // Regardless of the way Customer is initialized.
         }
 
         public Customer(int customerId)
         {
             CustomerId = customerId;
+            AdressList =  new List<Adress>();
         }
 
         public static int InstanceCount { get; set; }
-        public Adress homeAdress { get; set; }
-        public Adress wordAdress { get; set; }
+        public List<Adress> AdressList { get; set; }
         public int CustomerId { get; private set; }
         public string EmailAdress { get; set; }
         public string FirstName { get; set; }
